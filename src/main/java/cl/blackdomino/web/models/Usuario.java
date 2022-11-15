578997e435a5502eedcbf2f3385e5021ee9f5eb0
sebgarridor <sebgarridor@gmail.com>
2022-11-15 17:31:32 -0300

    Agregar datos a traves de Postman

--- src/main/java/cl/blackdomino/web/models/Usuario.java
+++ src/main/java/cl/blackdomino/web/models/Usuario.java
@@ -1,13 +1,49 @@
 package cl.blackdomino.web.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="usuario")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private int id;
+	
+	@NotNull
 	private String nombre;
+	
+	@NotNull
 	private String apellido;
+	
+	@NotNull
 	private String correo;
+	
+	@NotNull
 	private int telefono;
+	
+	
 	private int direccion_id;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt; //inserción de un registro
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt; //update de un registro
+	
 	public Usuario() {
 		super();
 	}
@@ -47,6 +83,15 @@ public class Usuario {
 	public void setDireccion_id(int direccion_id) {
 		this.direccion_id = direccion_id;
 	}
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 	
 
 }
