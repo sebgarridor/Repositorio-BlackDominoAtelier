ae07c6157f84dfef3429548843ee054ed9d7d7d8
sebgarridor <sebgarridor@gmail.com>
2022-11-16 16:47:01 -0300

    Repositories seba

---
+++ src/main/java/cl/blackdomino/web/repositories/DisenosRepository.java
@@ -0,0 +1,9 @@
+package cl.blackdomino.web.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.blackdomino.web.models.Disenos;
+
+public interface DisenosRepository extends JpaRepository<Disenos, Long>{
+
+}
---
+++ src/main/java/cl/blackdomino/web/repositories/TallajeRepository.java
@@ -0,0 +1,9 @@
+package cl.blackdomino.web.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.blackdomino.web.models.Tallaje;
+
+public interface TallajeRepository extends JpaRepository<Tallaje, Long>{
+
+}
