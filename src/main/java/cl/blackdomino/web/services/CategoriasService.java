baba5b90ad11832ad5d455255d49337b5cd6e66e
Poipurin <b.pinorojo@gmail.com>
2022-11-16 17:32:47 -0300

    Services: Categorías

---
+++ src/main/java/cl/blackdomino/web/services/CategoriasService.java
@@ -0,0 +1,7 @@
+package cl.blackdomino.web.services;
+
+import cl.blackdomino.web.models.Categorias;
+
+public interface CategoriasService {
+	public Categorias guardarCategorias(Categorias categoria);
+}
