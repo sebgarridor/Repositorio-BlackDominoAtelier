7c20790184e497f846acb6d682fb6c787b04373f
Poipurin <b.pinorojo@gmail.com>
2022-11-16 17:34:25 -0300

    Services: Productos

---
+++ src/main/java/cl/blackdomino/web/services/ProductosService.java
@@ -0,0 +1,7 @@
+package cl.blackdomino.web.services;
+
+import cl.blackdomino.web.models.Productos;
+
+public interface ProductosService {
+	public Productos guardarProductos(Productos producto);
+}
