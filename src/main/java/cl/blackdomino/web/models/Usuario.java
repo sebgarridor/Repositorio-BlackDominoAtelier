e21292183a50293fedbb01fd9cb12ccbb7aea88c
fati987 <maria.requena@mail.udp.cl>
2022-11-16 09:44:21 -0300

    fatima

--- src/main/java/cl/blackdomino/web/models/Usuario.java
+++ src/main/java/cl/blackdomino/web/models/Usuario.java
@@ -7,99 +7,91 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name="usuario")
 public class Usuario {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private int id;
+	
 	@NotNull
 	private String nombre;
-
+	
 	@NotNull
 	private String apellido;
-
+	
 	@NotNull
 	private String correo;
-
+	
 	@NotNull
-	@Range(min = 9, max = 11, message = "telefono invalido")
 	private int telefono;
-
-	@NotNull
+	
+	
 	private int direccion_id;
-
-	@Column(updatable = false)
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
-	private Date createdAt;
-
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
-	private Date updateAt;
-
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt; //inserción de un registro
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt; //update de un registro
+	
 	public Usuario() {
 		super();
 	}
-
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
-
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getApellido() {
 		return apellido;
 	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 	public String getCorreo() {
 		return correo;
 	}
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
 	public int getTelefono() {
 		return telefono;
 	}
-
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
-
 	public int getDireccion_id() {
 		return direccion_id;
 	}
-
 	public void setDireccion_id(int direccion_id) {
 		this.direccion_id = direccion_id;
 	}
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	
 
-	protected void onCreate() {
-		this.createdAt = new Date();
-	}
-
-	protected void onUpdate() {
-		this.updateAt = new Date();
-	}
 }
