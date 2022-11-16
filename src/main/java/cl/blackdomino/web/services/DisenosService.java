772f3cce8cd0c2f0b2823ee6bc74ff31844ae943
sebgarridor <sebgarridor@gmail.com>
2022-11-16 16:48:13 -0300

    Services seba

---
+++ src/main/java/cl/blackdomino/web/services/DisenosService.java
@@ -0,0 +1,9 @@
+package cl.blackdomino.web.services;
+
+import cl.blackdomino.web.models.Disenos;
+
+public interface DisenosService {
+	
+	public Disenos guardarDiseno(Disenos disenos);
+
+}
---
+++ src/main/java/cl/blackdomino/web/services/TallajeService.java
@@ -0,0 +1,9 @@
+package cl.blackdomino.web.services;
+
+import cl.blackdomino.web.models.Tallaje;
+
+public interface TallajeService {
+	
+	public Tallaje guardarTalla(Tallaje tallaje);
+
+}
