381b679273185574221cd7a7871a026c2cebfdb0
Poipurin <b.pinorojo@gmail.com>
2022-11-16 23:05:26 -0300

    Api Rest: Productos

--- src/main/java/cl/blackdomino/web/api/ProductosApiRestController.java
+++ src/main/java/cl/blackdomino/web/api/ProductosApiRestController.java
@@ -1,10 +1,13 @@
 package cl.blackdomino.web.api;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import cl.blackdomino.web.models.Productos;
 import cl.blackdomino.web.services.ProductosServiceImpl;
 
@@ -12,23 +15,41 @@ import cl.blackdomino.web.services.ProductosServiceImpl;
 public class ProductosApiRestController {
 	@Autowired
 	private ProductosServiceImpl productosServiceImpl;
-	
-	@RequestMapping ("/guardar/productos")
-	public Productos guardarProductos(@RequestBody Productos producto) {
-		
-		
+// -----------------------Guardar--------------------------------------------------------------------------
+	@RequestMapping ("/guardar/producto")
+	public Productos guardarProducto(@RequestBody Productos producto) {
 		// http://localhost:9080/guardar/productos
-		
 		/* nombre: "Totebag evangelion negra"
 		 * precio: "10000"
 		 *
-		 * 
-		 * 
-		 * 
-		 * 
 		 */
-		return productosServiceImpl.guardarProductos(producto); // 
+		return productosServiceImpl.guardarProducto(producto); // 
 		
 	}
+// -----------------------Eliminar--------------------------------------------------------------------------
+		@RequestMapping("/eliminar/producto")
+		public String eliminarProducto(@RequestParam(value = "id", required = false) Long id) {
+			return productosServiceImpl.eliminarProducto(id);
+		}
+
+// -----------------------Actualizar--------------------------------------------------------------------------
+		@RequestMapping("/actualizar/producto")
+		public String actualizarProducto(@RequestBody Productos producto) {
+			if (producto.getId() != null) {
+				productosServiceImpl.actualizarProducto(producto);
+			}
+			return "El producto no se actualizará";
+		}
+// -----------------------Obtener--------------------------------------------------------------------------
+		@RequestMapping("/obtener/producto")
+		public Optional<Productos> obtenerProducto(@RequestParam(value = "id", required = false) Long id) {
+			Optional<Productos> mensaje = productosServiceImpl.obtenerProducto(id);
+			return mensaje;
+		}
+//--------------------ObtenerLista----------------------------------
+		@RequestMapping("/listar/producto")
+		public List<Productos> obtenerListaProductos(){
+			return productosServiceImpl.obtenerListaProductos();
+		}
 }
 	
