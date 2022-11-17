e50b089aed49402e26d28247cb10d69495053454
Poipurin <b.pinorojo@gmail.com>
2022-11-16 23:51:22 -0300

    ServiceImpl: Categorías

--- src/main/java/cl/blackdomino/web/services/CategoriasServiceImpl.java
+++ src/main/java/cl/blackdomino/web/services/CategoriasServiceImpl.java
@@ -1,4 +1,7 @@
 package cl.blackdomino.web.services;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +12,47 @@ import cl.blackdomino.web.repositories.CategoriasRepository;
 public class CategoriasServiceImpl implements CategoriasService{
 	@Autowired
 	private CategoriasRepository categoriasRepository;
+	
+//--------------------Guardar----------------------------------
 	@Override
-	public Categorias guardarCategorias(Categorias categoria) {
-		
+	public Categorias guardarCategoria(Categorias categoria) {
 		return categoriasRepository.save(categoria);
+	}
+//--------------------Eliminar----------------------------------
+	@Override
+	public String eliminarCategoria(Long id) {
+		Boolean existe = categoriasRepository.existsById(id);
+		if (existe) {
+			categoriasRepository.deleteById(id);
+		} else {
+			return "La categoría no existe";
+		}
+		existe = categoriasRepository.existsById(id);
+		if (existe) {
+			return "La categoría no fue eliminada";
+		}
+		return "La categoría fue eliminada";
+	}
+//--------------------Actualizar----------------------------------
+	@Override
+	public String actualizarCategoria(Categorias categoria) {
+		Boolean existe = categoriasRepository.existsById(categoria.getId());
+		if (existe) {
+			categoriasRepository.save(categoria);
+			return "Categoría Actualizada";
+		}
+		return "Categoría no actualizada";
+	}
+	//--------------------Obtener----------------------------------		
+	@Override
+	public Optional<Categorias> obtenerCategoria(Long id) {
+		Optional<Categorias> mensaje = categoriasRepository.findById(id);
+		return mensaje;
+	}
+	//--------------------ObtenerLista----------------------------------
+	@Override
+	public List<Categorias> obtenerListaCategorias() {
+		return categoriasRepository.findAll();
 	}
 
 }
