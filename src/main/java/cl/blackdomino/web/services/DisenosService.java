8c5833c3a9f4ea813012c99406476f16ab3a750a
Poipurin <b.pinorojo@gmail.com>
2022-11-16 21:38:14 -0300

    Service: Diseño

--- src/main/java/cl/blackdomino/web/services/DisenosService.java
+++ src/main/java/cl/blackdomino/web/services/DisenosService.java
@@ -1,9 +1,15 @@
 package cl.blackdomino.web.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import cl.blackdomino.web.models.Disenos;
 
 public interface DisenosService {
 	
-	public Disenos guardarDiseno(Disenos disenos);
-
+	public Disenos guardarDiseno(Disenos diseno);
+	public String eliminarDiseno(Long id);
+	public String actualizarDiseno(Disenos diseno);
+	public Optional<Disenos> obtenerDiseno (Long id);
+	public List<Disenos> obtenerListaDisenos();
 }
