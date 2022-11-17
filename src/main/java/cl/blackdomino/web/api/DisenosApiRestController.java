3b4ac647e3d55fc5d29bad4d2bafbd2a5b2b66d8
Poipurin <b.pinorojo@gmail.com>
2022-11-16 23:00:18 -0300

    Api Rest: Diseño

--- src/main/java/cl/blackdomino/web/api/DisenosApiRestController.java
+++ src/main/java/cl/blackdomino/web/api/DisenosApiRestController.java
@@ -2,7 +2,6 @@ package cl.blackdomino.web.api;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,42 +13,44 @@ import cl.blackdomino.web.services.DisenosServiceImpl;
 
 @RestController
 public class DisenosApiRestController {
-	
+
 	@Autowired
 	private DisenosServiceImpl disenosServiceImpl;
-	//-----------------------Guardar--------------------------------------------------------------------------
-	@RequestMapping ("guardar/diseno")
+
+	// -----------------------Guardar--------------------------------------------------------------------------
+	@RequestMapping("guardar/diseno")
 	public Disenos guardarDiseno(@RequestBody Disenos diseno) {
 		// http://localhost:9080/guardar/disenos
 		return disenosServiceImpl.guardarDiseno(diseno);
-		
+
 	}
-	//-----------------------Eliminar--------------------------------------------------------------------------
+
+	// -----------------------Eliminar--------------------------------------------------------------------------
 	@RequestMapping("/eliminar/diseno")
-	public String eliminarDiseno(@RequestParam (value = "id", required = false)Long id) {
+	public String eliminarDiseno(@RequestParam(value = "id", required = false) Long id) {
 		return disenosServiceImpl.eliminarDiseno(id);
 	}
-	
-	//-----------------------Actualizar--------------------------------------------------------------------------
+
+// -----------------------Actualizar--------------------------------------------------------------------------
 	@RequestMapping("/actualizar/diseno")
 	public String actualizarDiseno(@RequestBody Disenos diseno) {
-		if (diseno.getId()!=null) {
+		if (diseno.getId() != null) {
 			disenosServiceImpl.actualizarDiseno(diseno);
 		}
 		return "El diseño no se actualizará";
 	}
 //-----------------------Obtener--------------------------------------------------------------------------
+
 	@RequestMapping("/obtener/diseno")
-	public Optional<Disenos> obtenerDiseno(@RequestParam(value="id",required = false) Long id
-			){
-		
+	public Optional<Disenos> obtenerDiseno(@RequestParam(value = "id", required = false) Long id) {
+
 		Optional<Disenos> mensaje = disenosServiceImpl.obtenerDiseno(id);
-				return mensaje;			
+		return mensaje;
 	}
+
 //--------------------ObtenerLista----------------------------------
 	@GetMapping("/listar/disenos")
-	public List<Disenos> ObtenerListaUsuarios(){
+	public List<Disenos> obtenerListaDisenos() {
 		return disenosServiceImpl.obtenerListaDisenos();
 	}
 }
-
