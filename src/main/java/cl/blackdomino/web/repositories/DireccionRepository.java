3e276b1bbb75c3e6cbb728d035a4e259ab999d36
gerald <geraldopitz@gmail.com>
2022-11-18 02:56:15 -0300

    repositories gerald

---
+++ src/main/java/cl/blackdomino/web/repositories/DireccionRepository.java
@@ -0,0 +1,9 @@
+package cl.blackdomino.web.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.blackdomino.web.models.Direccion;
+
+public interface DireccionRepository extends JpaRepository<Direccion, Long>{
+
+}
---
+++ src/main/java/cl/blackdomino/web/repositories/RolRepository.java
@@ -0,0 +1,10 @@
+package cl.blackdomino.web.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.blackdomino.web.models.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Long>{
+
+	
+}
