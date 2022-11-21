e0ba849718df944b505ea0742e5f7671d4dd3f26
gerald <geraldopitz@gmail.com>
2022-11-21 19:37:59 -0300

    Tabla ProductosVenta Gerald

--- src/main/java/cl/blackdomino/web/models/Venta.java
+++ src/main/java/cl/blackdomino/web/models/Venta.java
@@ -1,10 +1,18 @@
 package cl.blackdomino.web.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,5 +43,14 @@ public class Venta {
 	
 	private Integer totalVenta;
 	
+	//ManyToMany
+	@JsonIgnore
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+			name="productos_ventas",
+			joinColumns = @JoinColumn(name="venta_id"),
+			inverseJoinColumns = @JoinColumn(name="producto_id")			
+			)
+	private List<Producto> productos;
 	
 }
