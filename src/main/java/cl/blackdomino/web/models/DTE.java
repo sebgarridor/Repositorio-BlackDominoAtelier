39d050673194bed36058b0d4b211efddb98dc386
fati987 <maria.requena@mail.udp.cl>
2022-11-21 12:27:16 -0300

    lombokFatima

--- src/main/java/cl/blackdomino/web/models/DTE.java
+++ src/main/java/cl/blackdomino/web/models/DTE.java
@@ -12,9 +12,20 @@ import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Entity 
 @Table(name="dte")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class DTE {
 
 	@Id
