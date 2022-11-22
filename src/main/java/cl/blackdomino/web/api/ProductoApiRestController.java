645597bfe73fbefca4c255a0ed263cbc905f5df4
Poipurin <b.pinorojo@gmail.com>
2022-11-22 15:58:39 -0300

    ApiRest: Bárbara

--- src/main/java/cl/blackdomino/web/api/ProductoApiRestController.java
+++ src/main/java/cl/blackdomino/web/api/ProductoApiRestController.java
@@ -16,43 +16,46 @@ import cl.blackdomino.web.services.ProductoServiceImpl;
 public class ProductoApiRestController {
 	@Autowired
 	private ProductoServiceImpl productoServiceImpl;
+
 // -----------------------Guardar--------------------------------------------------------------------------
-	@RequestMapping ("/guardar/producto")
+	@RequestMapping("/guardar/producto")
 	public Producto guardarProducto(@RequestBody Producto producto) {
 		// http://localhost:9080/guardar/productos
-		/* nombre: "Totebag evangelion negra"
-		 * precio: "10000"
+		/*
+		 * nombre: "Totebag evangelion negra" precio: "10000"
 		 *
 		 */
-		return productoServiceImpl.guardarProducto(producto); // 
-		
+		return productoServiceImpl.guardarProducto(producto); //
+
 	}
+
 // -----------------------Eliminar--------------------------------------------------------------------------
-		@RequestMapping("/eliminar/producto")
-		public String eliminarProducto(@RequestParam(value = "id", required = false) Long id) {
-			return productoServiceImpl.eliminarProducto(id);
-		}
+	@RequestMapping("/eliminar/producto")
+	public String eliminarProducto(@RequestParam(value = "id", required = false) Long id) {
+		return productoServiceImpl.eliminarProducto(id);
+	}
 
 // -----------------------Actualizar--------------------------------------------------------------------------
-		@RequestMapping("/actualizar/producto")
-		public String actualizarProducto(@RequestBody Producto producto) {
-			if (producto.getId() != null) {
-				productoServiceImpl.actualizarProducto(producto);
-				String mensaje = productoServiceImpl.actualizarProducto(producto);
-				return mensaje;
-			}
-			return "El producto no se actualizará";
-		}
-// -----------------------Obtener--------------------------------------------------------------------------
-		@RequestMapping("/obtener/producto")
-		public Optional<Producto> obtenerProducto(@RequestParam(value = "id", required = false) Long id) {
-			Optional<Producto> mensaje = productoServiceImpl.obtenerProducto(id);
+	@RequestMapping("/actualizar/producto")
+	public String actualizarProducto(@RequestBody Producto producto) {
+		if (producto.getId() != null) {
+			productoServiceImpl.actualizarProducto(producto);
+			String mensaje = productoServiceImpl.actualizarProducto(producto);
 			return mensaje;
 		}
+		return "El producto no se actualizará";
+	}
+
+// -----------------------Obtener--------------------------------------------------------------------------
+	@RequestMapping("/obtener/producto")
+	public Optional<Producto> obtenerProducto(@RequestParam(value = "id", required = false) Long id) {
+		Optional<Producto> mensaje = productoServiceImpl.obtenerProducto(id);
+		return mensaje;
+	}
+
 //--------------------ObtenerLista----------------------------------
-		@GetMapping("/listar/producto")
-		public List<Producto> obtenerListaProductos(){
-			return productoServiceImpl.obtenerListaProductos();
-		}
+	@GetMapping("/listar/producto")
+	public List<Producto> obtenerListaProductos() {
+		return productoServiceImpl.obtenerListaProductos();
+	}
 }
-	
