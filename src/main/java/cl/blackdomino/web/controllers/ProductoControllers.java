5f5c51d90d07b21d5c8de89ca81cb3bb7727e18f
Poipurin <b.pinorojo@gmail.com>
2022-12-02 11:53:53 -0300

    rutas productos

--- src/main/java/cl/blackdomino/web/controllers/ProductoControllers.java
+++ src/main/java/cl/blackdomino/web/controllers/ProductoControllers.java
@@ -1,14 +1,36 @@
 package cl.blackdomino.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/producto")
 public class ProductoControllers {
-	@RequestMapping("/productos")
-	public String productos() {
-		System.out.println("en el método productos");
-		return "productos";
-	}
-}
 
+	// http://localhost:9080/producto/poleras
+	@GetMapping("/poleras")
+	public String mostrarPoleras() {
+		return "poleras.jsp";
+	}
+
+	// http://localhost:9080/producto/polerones
+	@GetMapping("/poleras")
+	public String mostrarPolerones() {
+		return "polerones.jsp";
+	}
+
+	// http://localhost:9080/producto/totebag
+	@GetMapping("/totebag")
+	public String mostrarTotes() {
+		return "totebag.jsp";
+	}
+	
+	// http://localhost:9080/producto/bandoleras
+		@GetMapping("/bandoleras")
+		public String mostrarBandoleras() {
+			return "bandoleras.jsp";
+		}
+	
+
+}
